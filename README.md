8a6ea855ba89d1ba7288a22828a5721b08e59b34
徐翔 <xuxiang.simple@bytedance.com>
2022-11-13 21:09:32 +0800

    It works

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pathz
-A command line utility to clean up $PATH variable for Linux and OS X.
+`pathz` remove duplicate entries from $PATH variable for Linux and OS X.
+You may put all your $PATH directories in `~/.paths` file, which is read by `pathz`,
+ignoring lines starting with `#`.
+
+The following pages helped me use stdlib utilities.
+- https://cplusplus.com/reference/set/set/set/
+- https://en.cppreference.com/w/cpp/container/vector
+- https://en.cppreference.com/w/cpp/string
+
+# build
+``g++ -Wall --std=c++20 main.cpp -o pathz``
+
+# use
+You may want to put the following line in your shell init script:
+```shell
+PATH=$(pathz)
+```
---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <vector>
+
+#define BLANKS " \t\r\n"
+#define COMMENT '#'
+#define SEP ':'
+
+
+using namespace std;
+
+// split str separated by sep into a vector
+vector<string> split(string str, char sep) {
+    vector<string> res;
+    size_t index = 0;
+    while (index < str.size()) {
+        size_t found = str.find_first_of(sep, index);
+        if (found == string::npos) {
+            res.push_back(str.substr(index));
+            break;
+        } else {
+            string p = str.substr(index, found-index);
+            res.push_back(p);
+            index = found + 1;
+        }
+    }
+    return res;
+}
+
+// trim leading and trailing spaces.
+string trim(string s) {
+    s = s.erase(0, s.find_first_not_of(BLANKS));
+    return s.erase(s.find_last_not_of(BLANKS)+1);
+}
+
+// read path directories from config file, line by line,
+// skipping lines that start with #
+vector<string> fromConfigFile(string file) {
+    vector<string> res;
+    fstream fin(file, ios::in);
+    if (fin.is_open()) {
+        for (string buf; getline(fin, buf); ) {
+            buf = trim(buf);
+            if (buf.starts_with(COMMENT)) {
+                continue;
+            }
+            res.push_back(buf);
+        }
+        fin.close();
+    } else {
+        cerr << "failed to read config file " << file  << endl;
+    }
+    return res;
+}
+
+// collect paths
+void addIfNotExists(set<string>& seen, vector<string>& res, vector<string>& tmp) {
+    for (auto it = tmp.begin(); it != tmp.end(); ++it) {
+        string p = *it;
+
+        if (seen.find(p) == seen.end()) {
+            seen.insert(p);
+            res.push_back(p);
+        }
+    }
+}
+
+int main (int argc, char* argv[]) {
+    string file = getenv("HOME");
+    file.append("/.pathz");
+
+    vector<string> paths = fromConfigFile(file);
+
+    set<string> seen;
+    vector<string> res;
+    addIfNotExists(seen, res, paths);
+
+    string env = getenv("PATH");
+
+    paths = split(env, SEP);
+    addIfNotExists(seen, res, paths);
+
+    bool first = true;
+    for (auto it = res.begin(); it != res.end(); ++it) {
+        if (first) {
+            first = false;
+        } else {
+            cout << ":" ;
+        }
+        cout << *it ;
+    }
+
+    return 0;
+}
+
